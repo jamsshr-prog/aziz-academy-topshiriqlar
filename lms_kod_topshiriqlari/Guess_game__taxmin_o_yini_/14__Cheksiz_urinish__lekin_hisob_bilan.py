fc25ff08501d46e0847e23463aa52f217c1946be
jamsshr-prog <jamsshr@gmail.com>
2026-05-28 14:25:59 +0300

    Add: 14) Cheksiz urinish, lekin hisob bilan [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/14__Cheksiz_urinish__lekin_hisob_bilan.py
@@ -0,0 +1,8 @@
+yashirin_son = 11
+urinishlar_soni = 0
+while True:
+    urinish = int(input())
+    urinishlar_soni += 1
+    if urinish == yashirin_son:
+        print(urinishlar_soni)
+        break
