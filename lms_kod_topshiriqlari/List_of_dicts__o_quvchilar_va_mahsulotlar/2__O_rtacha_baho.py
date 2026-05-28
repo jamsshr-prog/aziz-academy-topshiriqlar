fce7b2a2de2c7cb5c2371526f0e2c774a3d4e055
jamsshr-prog <jamsshr@gmail.com>
2026-05-28 14:39:53 +0300

    Add: 2) O‘rtacha baho [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/List_of_dicts__o_quvchilar_va_mahsulotlar/2__O_rtacha_baho.py
@@ -0,0 +1,11 @@
+n = int(input())
+students = []
+for _ in range(n):
+    name, score = input().split()
+    students.append({'name': name, 'score': int(score)})
+total_score = 0 
+for student in students:
+    total_score += student['score']
+average = total_score / n 
+print(average)
+
