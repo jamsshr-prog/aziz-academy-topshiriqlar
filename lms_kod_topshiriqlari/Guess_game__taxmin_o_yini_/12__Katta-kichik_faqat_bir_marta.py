0a9f37e04c5e95f7fe52fc818362a1f352125187
jamsshr-prog <jamsshr@gmail.com>
2026-05-28 14:25:51 +0300

    Add: 12) Katta-kichik faqat bir marta [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/12__Katta-kichik_faqat_bir_marta.py
@@ -0,0 +1,10 @@
+yashirin_son = 8
+while  True:
+    urinish = int(input())
+    if urinish == yashirin_son:
+        print("Correct")
+        break
+    elif urinish < yashirin_son:
+        print("Low")
+    else:
+        print("High")
