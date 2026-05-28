fc35bb46066cdd0fb70e8d7607f0fbd092ec99bc
jamsshr-prog <jamsshr@gmail.com>
2026-05-28 14:25:57 +0300

    Add: 13) Guess game (manfiy sonlar bilan) [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/13__Guess_game__manfiy_sonlar_bilan_.py
@@ -0,0 +1,10 @@
+yashirin_son = -4 
+while True:
+    urinish = int(input())
+    if urinish == yashirin_son:
+        print("Correct")
+        break
+    elif urinish < yashirin_son:
+        print("Low")
+    else:
+        print("High")
