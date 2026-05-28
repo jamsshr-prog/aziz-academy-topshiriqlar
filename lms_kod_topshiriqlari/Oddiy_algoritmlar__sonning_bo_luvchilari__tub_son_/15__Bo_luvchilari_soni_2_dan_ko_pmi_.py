daf77313571fd23c3532567a98c5073014098610
jamsshr-prog <jamsshr@gmail.com>
2026-05-28 14:25:12 +0300

    Add: 15) Bo‘luvchilari soni 2 dan ko‘pmi? [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Oddiy_algoritmlar__sonning_bo_luvchilari__tub_son_/15__Bo_luvchilari_soni_2_dan_ko_pmi_.py
@@ -0,0 +1,11 @@
+n = int(input())
+count = 0 
+for i in range(1, int(n ** 0.5) + 1):
+    if n % i == 0:
+        count += 1
+        if i != n // i:
+            count += 1
+if count > 2:
+    print("Yes")
+else:
+    print("No")
