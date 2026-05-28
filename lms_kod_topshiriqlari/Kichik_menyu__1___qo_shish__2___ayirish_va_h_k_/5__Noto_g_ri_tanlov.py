2e270e47acf5478143ccc4d5c5c8b3b3fb716321
jamsshr-prog <jamsshr@gmail.com>
2026-05-28 14:26:27 +0300

    Add: 5) Noto‘g‘ri tanlov [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/5__Noto_g_ri_tanlov.py
@@ -0,0 +1,12 @@
+a, b = map(int,input().split())
+tanlov = int(input())
+if tanlov == 1:
+    print(a + b)
+elif tanlov == 2:
+    print(a - b)
+elif tanlov == 3:
+    print(a * b)
+elif tanlov == 4:
+    print(a / b)
+else:
+    print("Invalid")
