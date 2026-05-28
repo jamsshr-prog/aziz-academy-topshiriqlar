5103906d29f81c507acefd4bbab6ca95868a827e
jamsshr-prog <jamsshr@gmail.com>
2026-05-28 14:12:29 +0300

    Add: 13) Chegirma (nested) [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Ichma-ich_shartlar__nested_if_/13__Chegirma__nested_.py
@@ -0,0 +1,10 @@
+price = float(input())
+if price >= 100:
+    if price >= 500:
+        price = price * 0.8
+    else:
+        price = price * 0.9
+else:
+    price = price 
+print(price)
+        
