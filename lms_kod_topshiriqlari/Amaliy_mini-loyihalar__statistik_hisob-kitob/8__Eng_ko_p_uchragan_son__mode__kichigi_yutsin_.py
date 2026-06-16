243f63c9bf2b06d56b4f30829a285a81b64b1058
jamsshr-prog <jamsshr@gmail.com>
2026-06-16 14:00:48 +0300

    Add: 8) Eng ko‘p uchragan son (mode, kichigi yutsin) [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Amaliy_mini-loyihalar__statistik_hisob-kitob/8__Eng_ko_p_uchragan_son__mode__kichigi_yutsin_.py
@@ -0,0 +1,7 @@
+numbers = [int(x) for x in input().split()]
+counts = {}
+for x in numbers:
+    counts[x] = counts.get(x, 0) + 1
+max_count = max(counts.values())
+modes = [x for x, count in counts.items() if count == max_count]
+print(min(modes))
