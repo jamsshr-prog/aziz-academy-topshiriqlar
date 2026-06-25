341ab369608e647bc492e84acabff97c21d55daf
jamsshr-prog <jamsshr@gmail.com>
2026-06-25 13:45:02 +0300

    Add: 11) div(a, b) — bo‘lish, b=0 bo‘lsa ERROR [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Funksiyaga_kirish_____def__parametr__return/11__div_a__b____bo_lish__b_0_bo_lsa_ERROR.py
@@ -0,0 +1,10 @@
+def div(a, b):
+    if b == 0:
+        return "ERROR"
+    return a / b 
+a, b = map(int, input().split())
+result = div(a, b)
+if result ==  "ERROR":
+    print("ERROR")
+else:
+    print(f"{result:.2f}")
