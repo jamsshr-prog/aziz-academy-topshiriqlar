0cd34f7d9b795729f40bd1a8f6a1082ed535df54
jamsshr-prog <jamsshr@gmail.com>
2026-06-25 13:34:29 +0300

    Add: 10) count_vowels(s) — unli harflar soni [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/Funksiyaga_kirish_____def__parametr__return/10__count_vowels_s____unli_harflar_soni.py
@@ -0,0 +1,9 @@
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0 
+    for ch in s:
+        if ch in vowels:
+            count += 1 
+    return count 
+s = input()
+print(count_vowels(s))
