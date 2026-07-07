e5da05ffcfb8cb176bd7fa8f7f9e9d9ddd11b33e
jamsshr-prog <jamsshr@gmail.com>
2026-07-07 11:25:06 +0300

    Add: Talaba ma'lumot kartasi [jamsshr-prog]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Talaba_ma_lumot_kartasi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Talaba_ma_lumot_kartasi.py
@@ -1,1 +1,8 @@
-print("ppp")
+ism = "Aziz Karimov"
+kurs = "Python"
+shahar = "Toshkent"
+print("=== KARTA ===")
+print("Ism:", ism)
+print("Kurs:", kurs)
+print("Shahar:", shahar)
+print("Ism uzunligi:", len(ism))
