0fea18741a3eaddea95166e2e755a05da7226791
jamsshr-prog <jamsshr@gmail.com>
2026-07-07 12:13:08 +0300

    Add: Haftalik davomat bayroqlari [jamsshr-prog]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Haftalik_davomat_bayroqlari.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Haftalik_davomat_bayroqlari.py
@@ -1,1 +1,10 @@
-print("sss")
+dushanba = True 
+seshanba = True 
+chorshanba = False 
+payshanba = True 
+juma = False 
+print(f"Dushanba: {dushanba}")
+print(f"Seshanba: {seshanba}")
+print(f"Chorshanba: {chorshanba}")
+print(f"Payshanba: {payshanba}")
+print(f"Juma: {juma}")
