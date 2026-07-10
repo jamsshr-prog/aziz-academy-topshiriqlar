3e4d666451e27ae2b8dbcb0a7be5912901f72979
jamsshr-prog <jamsshr@gmail.com>
2026-07-10 20:22:57 +0300

    Add: Ko'p bosqichli o'yin statistikasi [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/13-__Loyiha__Taxmin_o_yini__Guess_game____random___wh/01-Ko_p_bosqichli_o_yin_statistikasi.py
@@ -0,0 +1,17 @@
+r = int(input())
+jami = 0 
+eng_yaxshi = 10**9 
+for i in range(1, r + 1):
+    yashirin = int(input())
+    urinish = 0 
+    while True:
+        taxmin = int(input())
+        urinish += 1
+        if taxmin == yashirin:
+            break 
+    print(f"Round {i}: {urinish} urinish") 
+    jami += urinish 
+    if urinish < eng_yaxshi:
+        eng_yaxshi = urinish 
+print(f"Jami: { jami}")
+print(f"Eng yaxshi: {eng_yaxshi}")
