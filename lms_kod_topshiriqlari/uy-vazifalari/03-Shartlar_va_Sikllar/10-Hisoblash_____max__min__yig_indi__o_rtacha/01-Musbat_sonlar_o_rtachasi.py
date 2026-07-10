164887a88a924e102723ef2ca65267e45894d79d
jamsshr-prog <jamsshr@gmail.com>
2026-07-10 20:11:01 +0300

    Add: Musbat sonlar o'rtachasi [jamsshr-prog]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/10-Hisoblash_____max__min__yig_indi__o_rtacha/01-Musbat_sonlar_o_rtachasi.py
@@ -0,0 +1,12 @@
+n = int(input())
+yigindi = 0 
+soni = 0 
+for _ in range(n):
+    son = int(input())
+    if son > 0:
+        yigindi += son 
+        soni += 1 
+if soni == 0:
+    print(0)
+else:
+    print(yigindi // soni)
